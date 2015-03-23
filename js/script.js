2e51e4a523991065054824dd2dd15acc164d6c3e
Smoggy <avatar145@ukr.net>
2015-03-23 18:47:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var isLateralNavAnimating = false;
+	
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
