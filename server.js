8171b38e6c3571b52fe27faadd8f39198f387572
Smoggy <avatar145@ukr.net>
2015-04-03 17:34:47 +0300

    change package.json

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ var app = express();
 app.use(compression())
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address);
